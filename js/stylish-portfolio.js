2da64f3ecdf4725440fd8b5d1e669cabe5d8d229
Dane Finch <Dane@Danes-MacBook.local>
2018-10-19 20:03:09 -0700

    Firebase

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -61,31 +61,28 @@ error: function(data){
 }
 });
 
+var config = {
+  apiKey: "AIzaSyBFUP2NoYuBXSN-cIMKPoAh5CdDDBFt85M",
+  authDomain: "this-magic-moment.firebaseapp.com",
+  databaseURL: "https://this-magic-moment.firebaseio.com",
+  projectId: "this-magic-moment",
+  storageBucket: "this-magic-moment.appspot.com",
+  messagingSenderId: "401022451753"
+};
+firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var emailCounter = 0;
+
+  $("#click-button").on("click", function() {
+
+    emailCounter++;
+
+    database.ref().push({
+      emailCount: emailCounter,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+  });
 
 
-
-// Get the modal
-var modal = document.getElementById('ice-cream');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
